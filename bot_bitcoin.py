b1e57fd414e441539c44688ffa69cab7d29ae5bb
Leandro <leandro.naves123@gmail.com>
2025-04-01 20:37:56 -0300

    Atualização

--- bot_bitcoin.py
+++ bot_bitcoin.py
@@ -2,6 +2,25 @@ import ssl
 import json
 
 import websocket
+import bitstamp.client
+
+import credenciais
+
+
+def cliente():
+    return bitstamp.client.Trading(username=credenciais.USERNAME,
+                                   key=credenciais.KEY,
+                                   secret=credenciais.SECRET)
+
+
+def comprar(quantidade):
+    trading_client = cliente()
+    trading_client.buy_market_order(quantidade)
+
+
+def vender(quantidade):
+    trading_client = cliente()
+    trading_client.sell_market_order(quantidade)
 
 
 def ao_abrir(ws):
@@ -13,13 +32,14 @@ def ao_abrir(ws):
     "data": {
         "channel": "live_trades_btcusd"
     }
-}				
+}
 """
+
     ws.send(json_subscribe)
 
 
 def ao_fechar(ws):
-    print("fechou a conexão")
+    print("Fechou a conexão")
 
 
 def erro(ws, erro):
@@ -29,8 +49,15 @@ def erro(ws, erro):
 
 def ao_receber_mensagem(ws, mensagem):
     mensagem = json.loads(mensagem)
-    price = (mensagem['data']['price'])
+    price = mensagem['data']['price']
     print(price)
+
+    if price > 10000:
+        vender()
+    elif price < 8100:
+        comprar()
+    else:
+        print("Aguardar")
 
 
 if __name__ == "__main__":
@@ -39,5 +66,4 @@ if __name__ == "__main__":
                                 on_close=ao_fechar,
                                 on_message=ao_receber_mensagem,
                                 on_error=erro)
-
     ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
